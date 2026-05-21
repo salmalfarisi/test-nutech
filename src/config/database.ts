391437f915fe608b35ac944f8b2b8bd3caf82cbb
ubuntu <timetogetworkfaris@gmail.com>
2026-05-21 21:50:07 +0700

    final

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,12 +1,31 @@
+// import path from "path";
+// import "reflect-metadata";
+// import { DataSource } from "typeorm";
+
+// export const AppDataSource = new DataSource({
+//   type: "better-sqlite3",
+//   database: "./storage/database.sqlite",
+//   synchronize: false,
+//   logging: true,
+//   entities: [path.join(__dirname, "../../entities/**/*.{ts,js}")],
+//   migrations: [path.join(__dirname, "../../migrations/**/*.{ts,js}")],
+// });
+
 import path from "path";
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
-  type: "better-sqlite3",
-  database: "./storage/database.sqlite",
+  type: "postgres",
+  url: process.env.DATABASE_URL,
+
   synchronize: false,
   logging: true,
+
   entities: [path.join(__dirname, "../../entities/**/*.{ts,js}")],
   migrations: [path.join(__dirname, "../../migrations/**/*.{ts,js}")],
+
+  ssl: process.env.NODE_ENV === "production"
+    ? { rejectUnauthorized: false }
+    : false,
 });
